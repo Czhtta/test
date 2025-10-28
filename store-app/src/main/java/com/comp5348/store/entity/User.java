21d06d3672f926100b1893753806dfbdc4780880
Cong Zh <zhangcong@Mac.modem>
2025-10-28 20:43:40 +1100

    no.1 connection fix

--- store-app/src/main/java/com/comp5348/store/entity/User.java
+++ store-app/src/main/java/com/comp5348/store/entity/User.java
@@ -29,6 +29,6 @@ public class User {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Order> orders;
 
-    @Column(name = "address") // <-- 【新增】
+    @Column(name = "address")
     private String address;
 }
